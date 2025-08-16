76bce7ebde9eebedfe0478a7566bc4fb228348c9
syed shahmikh ali <syedshahmikh@gmail.com>
2025-08-16 15:43:07 +0500

    Update To_check_connectivity_of_ip_addresses.sh

--- Small Tasks For Understanding/To_check_connectivity_of_ip_addresses.sh
+++ Small Tasks For Understanding/To_check_connectivity_of_ip_addresses.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+IP=$( cat serverIP.csv | awk 'NR>1' | cut -d , -f2 )
 
+for i in $IP
+do
+	ping -c1 $i 1>> /home/username/file
+	status=$(echo $?)
+	if [[ $status -eq 0 ]]
+	then
+		echo -e "\n$i is connected\n"
+	else
+		echo -e "\n$i not connected\n"
+	fi
+done
